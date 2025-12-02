89879f8954e21e8b91e99931bdad0978d11284d3
GwenCodes <essexdesigns43.0@gmail.com>
2025-12-02 17:09:30 -0500

    Added useState

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
   function search(event) {
     event.preventDefault();
     alert("Searching...");
   }
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" placeholder="Type a word..." />
+        <input
+          type="search"
+          placeholder="Type a word..."
+          onChange={handleKeywordChange}
+        />
       </form>
     </div>
   );
